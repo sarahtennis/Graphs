d5dfb22b6e53a372685af2c28a564846c933554d
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-06 15:14:55 -0500

    creates text file maps for each provided graph

--- projects/adventure/world.py
+++ projects/adventure/world.py
@@ -33,6 +33,52 @@ class World:
             if 'w' in roomGraph[roomID][1]:
                 self.rooms[roomID].connectRooms('w', self.rooms[roomGraph[roomID][1]['w']])
         self.startingRoom = self.rooms[0]
+    
+    # prints rooms in world -- lecture code
+    def printRooms(self, filename):
+        rotatedRoomGrid = []
+        for i in range(0, len(self.roomGrid)):
+            rotatedRoomGrid.append([None] * len(self.roomGrid))
+        for i in range(len(self.roomGrid)):
+            for j in range(len(self.roomGrid[0])):
+                rotatedRoomGrid[len(self.roomGrid[0]) - j - 1][i] = self.roomGrid[i][j]
+        roomGrid = rotatedRoomGrid
+        map_str = ""
+        for row in roomGrid:
+            map_str += "#"
+            for room in row:
+                if room is not None and room.n_to is not None:
+                    map_str += "  |  "
+                else:
+                    map_str += "     "
+            map_str += "#\n"
+            # Print room row
+            map_str += "#"
+            for room in row:
+                if room is not None and room.w_to is not None:
+                    map_str += "-"
+                else:
+                    map_str += " "
+                if room is not None:
+                    map_str += f"{room.id}".zfill(3)
+                else:
+                    map_str += "   "
+                if room is not None and room.e_to is not None:
+                    map_str += "-"
+                else:
+                    map_str += " "
+            map_str += "#\n"
+            map_str += "#"
+            for room in row:
+                if room is not None and room.s_to is not None:
+                    map_str += "  |  "
+                else:
+                    map_str += "     "
+            map_str += "#\n"
+        # writes map of world to txt file
+        file = open(f"visual/{filename}.txt", "w")
+        file.write(map_str)
+        file.close()
 
 
 
